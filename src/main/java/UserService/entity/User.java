eefd01ac010fe03808d33f087e2d0ace971c9d56
Vhmnts <vhmnts.work@gmail.com>
2025-11-06 12:40:50 +0300

    fix minor changes

--- src/main/java/UserService/entity/User.java
+++ src/main/java/UserService/entity/User.java
@@ -2,7 +2,9 @@ package UserService.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -27,11 +29,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotBlank(message = "Name is mandatory")
+    @NotBlank(message = "Name shouldn't be empty")
+    @Size(min = 3, max = 100, message = "Name length should be between 3 and 100 characters")
     @Column(name = "name")
     private String name;
 
-    @NotBlank(message = "Surname is mandatory")
+    @NotBlank(message = "Surname shouldn't be empty")
+    @Size(min = 3, max = 100, message = "Surname length should be between 3 and 100 characters")
     @Column(name = "surname")
     private String surname;
 
@@ -40,6 +44,7 @@ public class User {
     private Date birthDate;
 
     @Email(message = "Email should be valid")
+    @Size(min = 6, max = 255, message = "Email length should be between 6 and 255 characters")
     @Column(unique = true, name = "email")
     private String email;
 
