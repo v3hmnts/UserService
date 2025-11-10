2a8a3e8205b8b578d0d78643760a8500d5660946
Vhmnts <vhmnts.work@gmail.com>
2025-11-10 10:59:00 +0300

    fix minor update

--- src/test/java/UserService/TestUserServiceApplication.java
+++ src/test/java/UserService/TestUserServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestUserServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.from(UserServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.from(UserServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
 
 }
